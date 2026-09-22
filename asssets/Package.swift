0c69b60fa314b7a6effe63b5370422329a0ecf3b
instinct-builds <wu57a5@mail.instinct.com>
2026-09-22 14:04:14 -0500

    ASSSETS 0.3: package bundled starter library
    Added resources to AsssetsApp target in Package.swift

--- asssets/Package.swift
+++ asssets/Package.swift
@@ -10,7 +10,11 @@ let package = Package(
     ],
     targets: [
         .target(name: "AsssetsCore"),
-        .executableTarget(name: "AsssetsApp", dependencies: ["AsssetsCore"]),
+        .executableTarget(
+            name: "AsssetsApp",
+            dependencies: ["AsssetsCore"],
+            resources: [.copy("StarterLibrary.zip")]
+        ),
         .testTarget(name: "AsssetsCoreTests", dependencies: ["AsssetsCore"]),
     ]
 )
